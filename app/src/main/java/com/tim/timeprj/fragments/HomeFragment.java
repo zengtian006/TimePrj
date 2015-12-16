229c4d816c089a2cd144735ce92df2bcdeb045d5
Zeng <zengtian006@gmail.com>
2015-12-16 11:46:37 +0800

    tim

--- app/src/main/java/com/tim/timeprj/fragments/HomeFragment.java
+++ app/src/main/java/com/tim/timeprj/fragments/HomeFragment.java
@@ -1,20 +1,29 @@
 package com.tim.timeprj.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mob.tools.gui.ViewPagerAdapter;
 import com.tim.timeprj.R;
+import com.tim.timeprj.component.CustomViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Zeng on 02/11/15.
  */
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
     private final static String TAG = HomeFragment.class.getSimpleName();
-
+    private TabLayout tabLayout;
+    public static CustomViewPager viewPager;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,8 +41,16 @@ public class HomeFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         findView(rootView);
         setListener();
-        // Inflate the layout for this fragment
+        setView();
         return rootView;
+    }
+
+    private void setView() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFrag(new SubScheduleFragment(), "Schedule");
+        adapter.addFrag(new SellFragment(), "Detail");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void setListener() {
@@ -41,6 +58,38 @@ public class HomeFragment extends Fragment{
     }
 
     private void findView(View rootView) {
-
+        viewPager = (CustomViewPager) rootView.findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
     }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/tim/timeprj/fragments/SubScheduleFragment.java
@@ -0,0 +1,23 @@
+package com.tim.timeprj.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tim.timeprj.R;
+
+/**
+ * Created by Zeng on 2015/12/16.
+ */
+public class SubScheduleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return rootView;
+    }
+}

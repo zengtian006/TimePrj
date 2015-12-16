d0e581e9e8b746777e76c90dae79c06c8243432a
Zeng <zengtian006@gmail.com>
2015-12-16 18:34:49 +0800

    tim

--- app/src/main/java/com/tim/timeprj/fragments/SubScheduleFragment.java
+++ app/src/main/java/com/tim/timeprj/fragments/SubScheduleFragment.java
@@ -17,7 +17,9 @@ import android.widget.Toast;
 import com.tim.timeprj.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by Zeng on 2015/12/16.
@@ -25,19 +27,12 @@ import java.util.Calendar;
 public class SubScheduleFragment extends Fragment {
     private final static String TAG = SubScheduleFragment.class.getSimpleName();
     RecyclerView rListView;
+    CustomAdapter customAdapter;
 
-    static String[] date_name;
-    static Integer[] date_img;
-
-    static String[] schedule_count = {
-            "1",
-            "9",
-            "2",
-            "3",
-            "4",
-            "5",
-            "7"
-    };
+    //    static String[] date_name;
+    List<Integer> date_img;
+    List<String> date_name;
+    List<Integer> schedule_count;
 
     @Nullable
     @Override
@@ -51,81 +46,93 @@ public class SubScheduleFragment extends Fragment {
     private void setView() {
         loadSchedule();
         rListView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rListView.setAdapter(new customAdapter(getActivity()));
-    }
-
-    private void loadSchedule() {
-        date_name = new String[7];
-        date_img = new Integer[7];
-        for (int i = 0; i < 7; i++) {
-            Calendar c = Calendar.getInstance();
-            c.add(Calendar.DATE, i);
-            SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-            String formattedDate = df.format(c.getTime());
-            date_name[i] = formattedDate;
-            if (i == 0) {
-                date_name[i] += " (today)";
-            }
-            SimpleDateFormat sdf = new SimpleDateFormat("EEE");
-            String dayofWeek = sdf.format(c.getTime());
-            if (dayofWeek.equals("Mon")) {
-                date_img[i] = R.drawable.monday;
-            } else if (dayofWeek.equals("Tue")) {
-                date_img[i] = R.drawable.tuesday;
-            } else if (dayofWeek.equals("Wed")) {
-                date_img[i] = R.drawable.wednesday;
-            } else if (dayofWeek.equals("Thu")) {
-                date_img[i] = R.drawable.thursday;
-            } else if (dayofWeek.equals("Fri")) {
-                date_img[i] = R.drawable.friday;
-            } else if (dayofWeek.equals("Sat")) {
-                date_img[i] = R.drawable.saturday;
-            } else if (dayofWeek.equals("Sun")) {
-                date_img[i] = R.drawable.sunday;
-            }
-            Log.v(TAG, "Formate time => " + formattedDate);
-            Log.v(TAG, "date of week => " + dayofWeek);
-        }
+        customAdapter = new CustomAdapter(getActivity());
+        rListView.setAdapter(customAdapter);
+        customAdapter.addItem();
     }
 
     private void findView(View rootView) {
         rListView = (RecyclerView) rootView.findViewById(R.id.date_list);
     }
 
-    private class customAdapter extends RecyclerView.Adapter<customAdapter.viewHolder> {
+    private void loadSchedule() {
+        schedule_count = new ArrayList<Integer>();
+        date_name = new ArrayList<String>();
+        date_img = new ArrayList<Integer>();
+        for (int i = 0; i < 7; i++) {
+            schedule_count.add(i);
+            Calendar c = Calendar.getInstance();
+            c.add(Calendar.DATE, i);
+            SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+            String formattedDate = df.format(c.getTime());
+            if (i == 0) {
+                formattedDate += " (today)";
+            }
+            date_name.add(formattedDate);
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE");
+            String dayofWeek = sdf.format(c.getTime());
+            if (dayofWeek.equals("Mon")) {
+                date_img.add(R.drawable.monday);
+            } else if (dayofWeek.equals("Tue")) {
+                date_img.add(R.drawable.tuesday);
+            } else if (dayofWeek.equals("Wed")) {
+                date_img.add(R.drawable.wednesday);
+            } else if (dayofWeek.equals("Thu")) {
+                date_img.add(R.drawable.thursday);
+            } else if (dayofWeek.equals("Fri")) {
+                date_img.add(R.drawable.friday);
+            } else if (dayofWeek.equals("Sat")) {
+                date_img.add(R.drawable.saturday);
+            } else if (dayofWeek.equals("Sun")) {
+                date_img.add(R.drawable.sunday);
+            }
+            Log.v(TAG, "Formate time => " + formattedDate);
+            Log.v(TAG, "date of week => " + dayofWeek);
+        }
+    }
+
+    private class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.viewHolder> {
 
         private final LayoutInflater inflater;
         private final Context mContext;
-        private final String[] item_name;
-        private final String[] item_count;
-        private final Integer[] img_id;
+        private final List<String> item_name;
+        private final List<Integer> item_count;
+        private final List<Integer> img_id;
 
-        public customAdapter(Context context) {
+        public CustomAdapter(Context context) {
             // TODO Auto-generated constructor stub
             inflater = LayoutInflater.from(context);
-            this.item_name = SubScheduleFragment.date_name;
-            this.item_count = SubScheduleFragment.schedule_count;
-            this.img_id = date_img;
+            this.item_name = SubScheduleFragment.this.date_name;
+            this.item_count = SubScheduleFragment.this.schedule_count;
+            this.img_id = SubScheduleFragment.this.date_img;
             this.mContext = context;
         }
 
         @Override
-        public customAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public CustomAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             return new viewHolder(inflater.inflate(R.layout.date_item, parent, false));
         }
 
+        public void addItem() {
+            this.item_name.add("test");
+            this.item_count.add(99);
+            this.img_id.add(R.drawable.monday);
+            notifyDataSetChanged();
+        }
+
         @Override
-        public void onBindViewHolder(customAdapter.viewHolder holder, int position) {
-            holder.mName.setText(item_name[position]);
-            holder.mImage.setImageResource(img_id[position]);
-            holder.mCount.setText(item_count[position]);
+        public void onBindViewHolder(CustomAdapter.viewHolder holder, int position) {
+            holder.mName.setText(item_name.get(position));
+            holder.mImage.setImageResource(img_id.get(position));
+            holder.mCount.setText(item_count.get(position).toString());
             holder.mDetail.setText("detail" + position);
         }
 
         @Override
         public int getItemCount() {
-            return SubScheduleFragment.date_name == null ? 0 : SubScheduleFragment.date_name.length;
+            return SubScheduleFragment.this.date_name == null ? 0 : SubScheduleFragment.this.date_name.size();
         }
+
 
         public class viewHolder extends RecyclerView.ViewHolder {
             public TextView mName;
@@ -142,12 +149,13 @@ public class SubScheduleFragment extends Fragment {
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Log.v(TAG, "position: " + item_name[getAdapterPosition()]);
-                        Toast.makeText(getActivity(), "click: " + item_name[getAdapterPosition()], Toast.LENGTH_SHORT).show();
+                        Log.v(TAG, "position: " + item_name.get(getAdapterPosition()));
+                        Toast.makeText(getActivity(), "click: " + item_name.get(getAdapterPosition()), Toast.LENGTH_SHORT).show();
                         HomeFragment.viewPager.setCurrentItem(1);
                     }
                 });
             }
         }
     }
+
 }
